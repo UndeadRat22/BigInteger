c1ddd63969d3480f95176de1db1394cdcd059417
Valdas Rakutis <36191877+UndeadRat22@users.noreply.github.com>
2018-12-11 11:46:14 +0200

    Update to latest version - memory leak fix.

--- LinkedList/List.h
+++ LinkedList/List.h
@@ -1,3 +1,9 @@
+/* Singly linked list, modified for usage alongside BigInteger
+AUTHOR: Tomas Zaicevas, Valdas Rakutis
+Software Engineering, Vilnius University, 1st year
+2018 Spring */
+#ifndef LIST_H
+#define LIST_H
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,39 +11,52 @@
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
+typedef enum {false, true} bool;
+
 typedef enum {
-	ALL_GOOD = 0,
-	OUT_OF_MEMORY_EXCEPTION = -1,
-	LIST_EMPTY_EXCEPTION = -2,
-	INDEX_OUT_OF_RANGE_EXCEPTION = -3,
-	DIVISION_BY_ZERO_EXCEPTION = -4
+    ALL_GOOD = 0,
+    OUT_OF_MEMORY_EXCEPTION = -1,
+    LIST_EMPTY_EXCEPTION = -2,
+    INDEX_OUT_OF_RANGE_EXCEPTION = -3,
+    DIVISION_BY_ZERO_EXCEPTION = -4
 } ErrorCode;
 
 typedef struct Node {
-	char data;
-	struct Node *next;
+    char data;
+    struct Node *next;
 } Node;
 
 typedef struct List {
-	Node *HEAD;
-	unsigned count;
+    Node *HEAD;
+    unsigned count;
 } List;
 
-List* string_to_list(char*);		/*returns the specified string a list of chars*/
-List* create_list(unsigned int size);				/*creates a list of the specified size (can be zero)
-													and returns the list of that size with all values
-													initialized to 0x00*/
-List* list_cpy(List*);								/*copies a given list to a new List (does not delete the old one)*/
-ErrorCode push_front(List*, char);                        /* push entry at the front of list in O(1) time */
-ErrorCode push_back(List*, char);                         /* push entry at the end of list in O(n) time */
-char pop_head(List*);                               /* returns first value of the list */
-char pop_tail(List*);                               /* returns last value of the list */
-Node* get_by_index(List*, int index);				/*returns the pointer to the specified Node*/
-ErrorCode insert(List*, int, char);                       /* insert entry by index [1, n] */
-ErrorCode delete_by_index(List*, int);                    /* delete entry by index [1, n] */
-char* list_to_string(List*, char optional);			/*converts a given List to a c-style string*/
-void free_list(List*);                              /* free memory and dissolve the list */
-void print_error(ErrorCode code);					/*prints error according the the error code*/
-List* get_longer(List* a, List* b, int* lenght);	/*returns the longer list of the two and it's lenght*/
-List* get_shorter(List* a, List* b, int* lenght);	/*returns the shorter list of the two and it's lenght*/
+/* ADD / REMOVE NODE */
+ErrorCode push_front(List*, char);                    /* push entry at the front of list */
+ErrorCode push_back(List*, char);                   /* push entry at the end of list */
+char pop_front(List*);                              /* returns first value of the list */
+char pop_back(List*);                               /* returns last value of the list */
+ErrorCode insert(List*, unsigned, char);            /* insert entry by index [1, n] */
+ErrorCode delete_by_index(List*, unsigned);         /* delete entry by index [1, n] */
+
+/* PARSE */
+List* string_to_list(char*);                        /* returns the specified string in a list of chars
+                                                     returns list with a single zero, if parsed string is not a number*/
+char* list_to_string(List*, char);                    /* converts a given List to a c string */
+void print_list(List);
+
+/* List manipulations */
+List* create_list(unsigned int size);                /* creates a list of a specified size (can be zero)
+                                                    and returns the list of that size with all values
+                                                    initialized to 0 */
+List* list_cpy(List*);                                /* copies a given list to a new List (does not delete the old one) */
 List* reverse_list(List *list);
+Node* get_by_index(List*, unsigned index);            /* returns the pointer to the specified Node */
+void free_list(List*);                              /* free memory and dissolve the list */
+void check_error(ErrorCode code);                    /* prints error according the the error code */
+bool is_empty(List);
+
+/* LOGICAL OPERATIONS */
+List* get_longer(List *a, List *b, int *length);    /* returns the longer list of the two and it's length */
+List* get_shorter(List *a, List *b, int *length);    /* returns the shorter list of the two and it's length */
+#endif
